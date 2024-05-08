414c6fd5fed7e67ac619e202802f36cf9562bb98
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2024-05-09 01:59:37 +0600

    pre define events

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ const {Server} = require('socket.io')
 
 const io = new Server(expressServer);
 
+//Pre define Events
+//Event Handling(Server Side)
+/*
+    .Connect 
+    .Message
+    .Disconnect
+    .Reconnect
+    .Ping
+    .Join
+    .Leave
+
+*/
+
+//Event Handling(Client Side)
+/* 
+    .Connect
+    .Connect_error
+    .Connect_timeout
+    .Reconnect
+
+*/
+
 io.on('connection', (socket)=>{
     console.log('New user Connected'),
 
